0099012b56143e258559ecbcfa4324b7992d864e
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2021-04-06 19:16:09 +0000

    updated lambda/index.js.

--- lambda/index.js
+++ lambda/index.js
@@ -122,7 +122,8 @@ const RemainingIntentHandler = {
         return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
             && Alexa.getIntentName(handlerInput.requestEnvelope) === 'RemainingIntent';
     },
-   const s = handlerInput.requestEnvelope.request.intent.slots[0];
+     handle(handlerInput) {
+        const s = handlerInput.requestEnvelope.request.intent.slots[0];
         const location = (s.Location.value) ? s.Location.value : null;
         console.log('remaining location = ' + location);
         const now = new Date();
