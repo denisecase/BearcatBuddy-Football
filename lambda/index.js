98a6acbce8d00b79b8962573b52c37462025be40
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2021-04-09 03:22:42 +0000

    updated lambda/index.js.

--- lambda/index.js
+++ lambda/index.js
@@ -1,6 +1,8 @@
-// This sample demonstrates handling intents from an Alexa skill using the Alexa Skills Kit SDK (v2).
-// Please visit https://alexa.design/cookbook for additional examples on implementing slots, dialog management,
-// session persistence, api calls, and more.
+/* * MODIFIED FOR BEARCAT BUDDY***************************************
+ * This sample demonstrates handling intents from an Alexa skill using the Alexa Skills Kit SDK (v2).
+ * Please visit https://alexa.design/cookbook for additional examples on implementing slots, dialog management,
+ * session persistence, api calls, and more.
+ * */
 const Alexa = require('ask-sdk-core');
 
 // constants for our custom skill
@@ -10,24 +12,27 @@ const sport = 'Football';
 const home = 'home';
 const homecity = 'Maryville, Missouri';
 const away = 'away';
+const intentText1 = 'when is the next game';
+const intentText2 = 'how many games remaining'; 
 const helpText = 'Say when is the next game or how many games remaining? Say stop or cancel to exit.';
 const doneText = 'Go Bearcats - Goodbye!';
 const errorText = "Sorry, I could not understand. Please try again.";
+const victoryText = "";
 
 // games
-const games = require('./games')
+const games = require('./games');
 
 // helper function - get next 
 function getNext(now, location) {
     console.log('Entering getNext: ' + now + ' ' + location);
     const ls = (location === null) ? "" : location;
-    let ans = 'There are no more ' + gender + ' ' + sport + ' games in the current schedule. ';
+    let ans = 'There are no more ' + gender + ' ' + sport + ' games in the current schedule. ' + victoryText;
     for (let j = 0; j < games.length; j++) {
         const item = games[j];
         const p = item.gamelocation;
         const i = item.gamedate;
         const d = new Date(i.year, i.month - 1, i.day, i.hour, i.minute, 0);
-        const t = (0 === i.hour) ? "a time to be determined" : d.toLocaleTimeString('en-US')
+        const t = (0 === i.hour) ? "a time to be determined" : d.toLocaleTimeString('en-US');
         if (d > now) {
             if (location === null) {
                 ans = 'The next ' + gender + ' ' + sport + ' game is ' + d.toDateString() + ' at ' + t + ' in ' + p + ".";
@@ -84,7 +89,7 @@ const LaunchRequestHandler = {
         return Alexa.getRequestType(handlerInput.requestEnvelope) === 'LaunchRequest';
     },
     handle(handlerInput) {
-        const speakOutput = 'Welcome from ' + skillName + '. Say When is the next home game? or How many games remaining?';
+        const speakOutput = `Welcome from ${skillName}. Say ${intentText1} or ${intentText2}`;
         return handlerInput.responseBuilder
             .speak(speakOutput)
             .reprompt(speakOutput)
@@ -107,7 +112,9 @@ const NextIntentHandler = {
         console.log('next now = ' + now.toDateString());
         let speechText = '';
         speechText = speechText + ' ' + getNext(now, location);
-        if (speechText === '') { speechText = "Hmm...I can't help with that one. Try asking when is the next game or how many games remaining?" }
+        if (speechText === '') { 
+            speechText = `Hmm...I can't help with that one. Try asking ${intentText1} or ${intentText2}`;
+        }
         return handlerInput.responseBuilder
             .speak(speechText)
             .reprompt('Ask how many games remaining or say stop or cancel to exit.')
@@ -130,7 +137,9 @@ const RemainingIntentHandler = {
         console.log('remaining now = ' + now.toDateString());
         let speechText = '';
         speechText = speechText + ' ' + getRemaining(now, location);
-        if (speechText === '') { speechText = "Hmm...I can't help with that one. Say when is the next game or how many games remaining?" }
+         if (speechText === '') { 
+            speechText = `Hmm...I can't help with that one. Try asking ${intentText1} or ${intentText2}`;
+        }
         return handlerInput.responseBuilder
             .speak(speechText)
             .reprompt('Ask when is the next game or say stop or cancel to exit.')
@@ -170,46 +179,19 @@ const CancelAndStopIntentHandler = {
     }
 };
 
-const SessionEndedRequestHandler = {
+/* *
+ * FallbackIntent triggers when a customer says something that doesn’t map to any intents in your skill
+ * It must also be defined in the language model (if the locale supports it)
+ * This handler can be safely added but will be ingnored in locales that do not support it yet 
+ * */
+const FallbackIntentHandler = {
     canHandle(handlerInput) {
-        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'SessionEndedRequest';
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
+            && Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.FallbackIntent';
     },
     handle(handlerInput) {
-        // Any cleanup logic goes here.
-        return handlerInput.responseBuilder.getResponse();
-    }
-};
-
-// The intent reflector is used for interaction model testing and debugging.
-// It will simply repeat the intent the user said. You can create custom handlers
-// for your intents by defining them above, then also adding them to the request
-// handler chain below.
-const IntentReflectorHandler = {
-    canHandle(handlerInput) {
-        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest';
-    },
-    handle(handlerInput) {
-        const intentName = Alexa.getIntentName(handlerInput.requestEnvelope);
-        // const speakOutput = `You just triggered ${intentName}`;
-        const speakOutput = `The ${skillName} skill can't help with that. ${helpText}`;
-        return handlerInput.responseBuilder
-            .speak(speakOutput)
-            //.reprompt('add a reprompt if you want to keep the session open for the user to respond')
-            .getResponse();
-    }
-};
-
-// Generic error handling to capture any syntax or routing errors. If you receive an error
-// stating the request handler chain is not found, you have not implemented a handler for
-// the intent being invoked or included it in the skill builder below.
-const ErrorHandler = {
-    canHandle() {
-        return true;
-    },
-    handle(handlerInput, error) {
-        console.log(`~~~~ Error handled: ${error.stack}`);
-        // const speakOutput = `Sorry, I had trouble doing what you asked. Please try again.`;
-        const speakOutput = errorText;
+        // const speakOutput = 'Sorry, I don\'t know about that. Please try again.';
+        const speakOutput = helpText;
         return handlerInput.responseBuilder
             .speak(speakOutput)
             .reprompt(speakOutput)
@@ -217,16 +199,76 @@ const ErrorHandler = {
     }
 };
 
-// The SkillBuilder acts as the entry point for your skill, routing all request and response
-// payloads to the handlers above. Make sure any new handlers or interceptors you've
-// defined are included below. The order matters - they're processed top to bottom.
+/* *
+ * SessionEndedRequest notifies that a session was ended. This handler will be triggered when a currently open 
+ * session is closed for one of the following reasons: 1) The user says "exit" or "quit". 2) The user does not 
+ * respond or says something that does not match an intent defined in your voice model. 3) An error occurs 
+ * */
+const SessionEndedRequestHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'SessionEndedRequest';
+    },
+    handle(handlerInput) {
+        console.log(`~~~~ Session ended: ${JSON.stringify(handlerInput.requestEnvelope)}`);
+        // Any cleanup logic goes here.
+        return handlerInput.responseBuilder.getResponse(); // notice we send an empty response
+    }
+};
+
+/* *
+ * The intent reflector is used for interaction model testing and debugging.
+ * It will simply repeat the intent the user said. You can create custom handlers for your intents 
+ * by defining them above, then also adding them to the request handler chain below 
+ * */
+const IntentReflectorHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest';
+    },
+    handle(handlerInput) {
+        const intentName = Alexa.getIntentName(handlerInput.requestEnvelope);
+        const speakOutput = `You just triggered ${intentName}`;
+
+        return handlerInput.responseBuilder
+            .speak(speakOutput)
+            //.reprompt('add a reprompt if you want to keep the session open for the user to respond')
+            .getResponse();
+    }
+};
+
+/**
+ * Generic error handling to capture any syntax or routing errors. If you receive an error
+ * stating the request handler chain is not found, you have not implemented a handler for
+ * the intent being invoked or included it in the skill builder below 
+ * */
+const ErrorHandler = {
+    canHandle() {
+        return true;
+    },
+    handle(handlerInput, error) {
+        // const speakOutput = `Sorry, I had trouble doing what you asked. Please try again.`;
+        const speakOutput = errorText;
+        console.log(`~~~~ Error handled: ${JSON.stringify(error)}`);
+        return handlerInput.responseBuilder
+            .speak(speakOutput)
+            .reprompt(speakOutput)
+            .getResponse();
+    }
+};
+
+/**
+ * This handler acts as the entry point for your skill, routing all request and response
+ * payloads to the handlers above. Make sure any new handlers or interceptors you've
+ * defined are included below. The order matters - they're processed top to bottom 
+ * */
 exports.handler = Alexa.SkillBuilders.custom()
     .addRequestHandlers(
         LaunchRequestHandler,
         NextIntentHandler,  //  add custom intents and remove any unnecessary ones (e.g. hello world)
         RemainingIntentHandler, //  add custom intents and remove any unnecessary ones (e.g. hello world)
+        // HelloWorldIntentHandler,
         HelpIntentHandler,
         CancelAndStopIntentHandler,
+        FallbackIntentHandler,
         SessionEndedRequestHandler,
         IntentReflectorHandler, // make sure IntentReflectorHandler is last so it doesn't override your custom intent handlers
     )
@@ -234,3 +276,4 @@ exports.handler = Alexa.SkillBuilders.custom()
         ErrorHandler,
     )
     .lambda();
+    
